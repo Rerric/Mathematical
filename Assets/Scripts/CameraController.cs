7355b0ce7b038d5954839abc2f6c11a71211fe1f
Rerric <79161388+Rerric@users.noreply.github.com>
2022-03-24 13:41:52 -0400

    Adjusted Camera FOV Controls

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public Camera myCamera;
+
+    public float zoomSpeed;
+
+    public float maxFOV;
+    public float minFOV;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,22 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            myCamera.fieldOfView -= zoomSpeed * Time.deltaTime;
+            if (myCamera.fieldOfView < minFOV)
+            {
+                myCamera.fieldOfView = minFOV;
+            }
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            myCamera.fieldOfView += zoomSpeed * Time.deltaTime;
+            if (myCamera.fieldOfView > maxFOV)
+            {
+                myCamera.fieldOfView = maxFOV;
+            }
+        }
     }
 }
