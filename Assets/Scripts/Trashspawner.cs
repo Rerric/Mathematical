eaf1b01ba4883e78dd897ca4230c2c0752f1e01a
Tenichs <79172434+Tenichs@users.noreply.github.com>
2022-05-04 20:14:19 -0400

    trashspawner works now

--- Assets/Scripts/Trashspawner.cs
+++ Assets/Scripts/Trashspawner.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class Trashspawner : MonoBehaviour
 {
     public GameObject[] trashspawner;
-
+    public Vector3 spawnPos = new Vector3(0, 0, 0);
     public int ChooseSide = 0;
     public Addictionlevel AddictScript;
-    Vector3 spawnPos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -37,29 +37,28 @@ public class Trashspawner : MonoBehaviour
             {
                 int index = Random.Range(0, trashspawner.Length);
                 //chooses on which side of desk to spawn
-              
 
-                Invoke("SpawnSides", 2);
-               
+
+                
+                ChooseSide = Random.Range(0, 2);
+                if (ChooseSide > 0)
+                {
+                    spawnPos = new Vector3(Random.Range(-15, -5), 2.5f, Random.Range(6, 14));
+                    Debug.Log("leftchosen");
+                }
+                else
+                {
+                     spawnPos = new Vector3(Random.Range(7, 13), 2.5f, Random.Range(6, 14));
+                    Debug.Log("rightchosen");
+                }
+
                 Instantiate(trashspawner[index], spawnPos, trashspawner[index].transform.rotation);
                 AddictScript.messy -= 1;
                 Debug.Log("trashWorking");
             }
         }
     }
-    //Chooses which side of desk to spawn trash
-    void SpawnSides()
-    {
-        ChooseSide = Random.Range(0, 2);
-        if (ChooseSide >= 1)
-        {
-            Vector3 spawnPos = new Vector3(Random.Range(-15, -5), 2.5f, Random.Range(6, 14));
-            Debug.Log("check");
-        }
-        if(ChooseSide==0)
-        {
-            Vector3 spawnPos = new Vector3(Random.Range(7, 13), 2.5f, Random.Range(6, 14));
-            Debug.Log("other");
-        }
-    }
+
+   
+    
 }
