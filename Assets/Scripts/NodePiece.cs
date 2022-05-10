4dff20d707f2ebb4ff68c73e553580633fe71018
Rerric <79161388+Rerric@users.noreply.github.com>
2022-05-10 11:30:05 -0400

    Match3 Fixes
    Still not working as it did before but the images are appearing

---
+++ Assets/Scripts/NodePiece.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NodePiece : MonoBehaviour
+{
+    public int value;
+    public Point index;
+
+    [HideInInspector]
+    public Vector2 pos;
+    [HideInInspector]
+    public RectTransform rect;
+
+    Image img;
+
+    public void Initialize(int v, Point p, Sprite piece)
+    {
+        img = GetComponent<Image>();
+        rect = GetComponent<RectTransform>();
+
+        value = v;
+        SetIndex(p);
+        img.sprite = piece;
+    }
+
+    public void SetIndex(Point p)
+    {
+        index = p;
+        ResetPosition();
+        UpdateName();
+    }
+
+    public void ResetPosition()
+    {
+        pos = new Vector2(0.4f + (0.8f * index.x), -0.4f - (0.8f * index.y));
+    }
+
+    void UpdateName()
+    {
+        transform.name = "Node [" + index.x + ", " + index.y + "]";
+    }
+}
